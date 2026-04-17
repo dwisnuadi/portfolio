c928b90bff7ff71ad43e73b35378c6e16d5661bf
dwisnuadi <dwisnua71@gmail.com>
2026-04-17 19:13:16 +0800

    backend/server.js

--- backend/server.js
+++ backend/server.js
@@ -32,6 +32,8 @@ app.get("/projects", async (req, res) => {
 });
 
 
-app.listen(3001, () => {
-  console.log("Server running on http://localhost:3001");
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
